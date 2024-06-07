b357c2c8ca40b19276dac25532bb7fcde45640c5
Tim_Ludwiczak <tim.ludwiczak@gmail.com>
2024-06-07 16:24:31 -0500

    sorta done

--- terminal_phone_tasks.py
+++ terminal_phone_tasks.py
@@ -1,16 +1,40 @@
 # Terminal Phone
+# Terminal phone
+# Terminal Phone
+
 current_tasks = []
+prompt = input("What would you like to do with your tasks? Create, update, delete, or read? When done type exit")
+
 def phone_tasks(current_tasks):
     command = ''
     while(command != 'exit'):
         command = input()
         if command == 'create':
             print('You wish you could create')
+            task = input('Name your precious task')
+            current_tasks.append(task)
+            print(f'{task} is a terrible name')
         elif command == 'update':
             print('Update who?')
+            task_name = input('At least tell me the name')
+            if task_name in current_tasks:
+                new_task = input('What should the new terrible name be?')
+                current_tasks[task] = new_task
+            else:
+                print ('nothing that terrible exist, sorry')
         elif command == 'delete':
             print('Hasta la vista, Baby')
+            task_name = input('What garbage will we be deleting?')
+            if task_name in current_tasks:
+                current_tasks.remove(task_name)
+                print(f'Finally, {task_name} is out of my life')
+            else:
+                print('That poopy is long gone my friend')
         elif command == 'read':
-            'Brandon loves to read'
+            print('Brandon loves to read')
+            print(current_tasks)
+        elif command == 'exit':
+            print('Finally I can go about my day')
         else:
-            'Enter something that works'
+            'Enter something that works like creat or update or something idk'
+        
