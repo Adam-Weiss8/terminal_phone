eb1998f0ba24878887c728bc5db3c56ef729b1d4
byeoh100 <rax745@gmail.com>
2024-06-07 15:44:58 -0500

    Add search history saving

--- feature-internet-search.py
+++ feature-internet-search.py
@@ -1,4 +1,5 @@
 import webbrowser
+import os.path
 
 def search_engine_choice():
     choice = input("Select a search engine:\n1 for Google\n2 for Bing\n")
@@ -19,9 +20,20 @@ def search_terms():
             search_filtered += "+"
         else:
             search_filtered += i
+    search_history(search_raw)
     return search_filtered
+
+def search_history(search):
+    file_path = os.path.abspath(".") + "/history.txt"
+    if not os.path.isfile(file_path):
+        open("history.txt", "x")
+    with open(file_path, "a") as file:
+        file.write(search)
+
 
 def search_system():
     browser = search_engine_choice()
     search_term = search_terms()
-    webbrowser.open(f"https://www.{browser}.com/search?q=" + search_term, new=2)
+    #webbrowser.open(f"https://www.{browser}.com/search?q=" + search_term, new=2)
+
+search_history("a")
