4e999f73a9920b65800eb463e11c745c33222a8e
mickey_gardner <mickeygardner85@gmail.com>
2024-06-08 13:29:53 -0500

    add colors to menu

--- phone_menu_interface.py
+++ phone_menu_interface.py
@@ -6,20 +6,20 @@
 # "Tasks and Subtasks" dropdown should have Create, Update, Delete, Read
 
 # get input from user, if contacts, link to contacts, etc.
-from feature-internet-search import search_system
-from feature-manage-contacts import manage_contacts
+from feature_internet_search import search_system
+from feature_manage_contacts import manage_contacts
 from tasks_features import phone_tasks
 
-print("Hello, and welcome to your phone main menu. Please make a selection.")
+print("[green]Hello, and welcome to your phone main menu. Please make a selection.[/green]")
 user_input = ''
 while user_input != "Quit":
  print("""
     Enter the number for your selection
-     1. Contacts
-     2. Internet
-     3. Tasks and Subtasks
+     [green]1.[/green] [blue]Contacts[/blue]
+     [green]2.[/green] [red]Internet[/red]
+     [green]3.[/green] [purple]Tasks and Subtasks[/purple]
  """)
-    user_input = input("Enter your selection: ")
+    user_input = input("[green]Enter your selection:[/green] ")
  if user_input == "Contacts":
   manage_contacts()
  elif user_input == "Internet":
@@ -29,5 +29,5 @@ while user_input != "Quit":
  elif user_input == "Quit":
   print("Exiting Menu")
  else:
-  print("Invalid Selection")
+  print("[red]Invalid Selection[/red]")
 
